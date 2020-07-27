7a6c4ca98bd5dc2659f8b8b69e762492387b2e62
Ivan <929020287@qq.com>
2020-07-27 15:57:41 +0800

    1.7.2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[allow(clippy::float_cmp)]
+extern crate rand;
 mod vec3;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
@@ -9,7 +10,17 @@ pub use image::Rgb;
 pub use ray::Ray;
 mod hit;
 use hit::*;
+mod camera;
+use camera::Camera;
 pub const INF: f64 = std::f64::INFINITY;
+use rand::prelude::*;
+
+
+pub fn get_rand(low:f64, high:f64) -> f64{
+    let mut rng = rand::thread_rng();
+    let x: f64 = rng.gen(); // generates a float between 0 and 1
+    x * (high - low) + low
+}
 
 /*fn hit_sphere(center: Vec3, radius: f64, this_ray: &Ray) -> f64 {
     let a = this_ray.dir * this_ray.dir;
@@ -41,35 +52,38 @@ fn main() {
     let image_width = ((image_height as f64) * aspect_ratio) as u32;
     let mut img: RgbImage = ImageBuffer::new(image_width, image_height);
     let bar = ProgressBar::new(image_width as u64);
+    const samples_per_pixel: i32 = 100;
 
-    let viewport_height = 2;
-    let viewport_width = ((viewport_height as f64) * aspect_ratio) as u32;
-    let focal_length = 1;
+    // let viewport_height = 2;
+    // let viewport_width = ((viewport_height as f64) * aspect_ratio) as u32;
+    // let focal_length = 1;
 
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width as f64, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height as f64, 0.0);
-    let upper_left_corner =
-        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length as f64);
+    // let origin = Vec3::new(0.0, 0.0, 0.0);
+    // let horizontal = Vec3::new(viewport_width as f64, 0.0, 0.0);
+    // let vertical = Vec3::new(0.0, viewport_height as f64, 0.0);
+    // let lower_left_corner =
+    //     origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length as f64);
 
     let mut world: HittableList = HittableList::default();
     world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
+    let cam: Camera = Camera :: new();
+
     for x in 0..image_width {
         for y in 0..image_height {
             let pixel = img.get_pixel_mut(x, image_height - 1 - y);
-            let dx = (x as f64) / (image_width as f64);
-            let dy = (y as f64) / (image_height as f64);
-            let this_ray = Ray::new(
-                origin,
-                upper_left_corner + horizontal * dx + vertical * dy - origin,
-            );
-            let color = get_color(&this_ray, &world);
+            let mut color:Vec3 = Vec3 :: zero();
+            for i in 0..samples_per_pixel{
+                let dx = (x as f64 + get_rand(0.0, 1.0)) / (image_width as f64);
+                let dy = (y as f64 + get_rand(0.0, 1.0)) / (image_height as f64);
+                let this_ray = cam.get_ray(dx, dy);
+                color = color + get_color(&this_ray, &world) / samples_per_pixel as f64 ;
+            }
             *pixel = Rgb([
-                (color.x * 255.0) as u8,
-                (color.y * 255.0) as u8,
-                (color.z * 255.0) as u8,
+                (color.x as f64 * 255.0) as u8,
+                (color.y as f64 * 255.0) as u8,
+                (color.z as f64 * 255.0) as u8,
             ]);
         }
         bar.inc(1);
